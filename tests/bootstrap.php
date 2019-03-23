1fecc7283186c37688c2ebe4ec6eff45724b5006
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2019-03-23 16:31:08 +0100

    Add PHPUnit Composer dependency

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the phpstan-magento package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+// include and configure Composer autoloader
+include __DIR__ . '/../vendor/autoload.php';
