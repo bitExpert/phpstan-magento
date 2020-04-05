3f32a0378e63475b50a68677e7d957e7c24b8ee1
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2020-04-05 12:34:20 +0200

    Fix typo

--- autoload.php
+++ autoload.php
@@ -9,6 +9,6 @@
  * file that was distributed with this source code.
  */
 
-// Include this file if you are using the phar version of PHPStan. Since the phar version make use of dynamic namespaces
+// Include this file if you are using the phar version of PHPStan. Since the phar version makes use of dynamic namespaces
 // the hack in registration.php to overload spl_autoload_register() and spl_autoload_unregister() does not work any more.
 \bitExpert\PHPStan\Magento\Autoload\Autoloader::register();
