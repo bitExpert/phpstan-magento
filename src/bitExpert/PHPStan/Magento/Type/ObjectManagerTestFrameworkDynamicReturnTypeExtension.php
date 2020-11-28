56594dde9e6411cce0f85e747012f76e39ce027a
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2020-11-28 14:32:19 +0100

    Add type for ObjectManager helper for tests

---
+++ src/bitExpert/PHPStan/Magento/Type/ObjectManagerTestFrameworkDynamicReturnTypeExtension.php
@@ -0,0 +1,136 @@
+<?php
+
+/*
+ * This file is part of the phpstan-magento package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace bitExpert\PHPStan\Magento\Type;
+
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\ShouldNotHappenException;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+
+class ObjectManagerTestFrameworkDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+    /**
+     * @return string
+     */
+    public function getClass(): string
+    {
+        return 'Magento\Framework\TestFramework\Unit\Helper\ObjectManager';
+    }
+
+    /**
+     * @param MethodReflection $methodReflection
+     * @return bool
+     */
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return in_array(
+            $methodReflection->getName(),
+            ['getObject', 'getConstructArguments', 'getCollectionMock'],
+            true
+        );
+    }
+
+    /**
+     * @param MethodReflection $methodReflection
+     * @param MethodCall $methodCall
+     * @param Scope $scope
+     * @return Type
+     */
+    public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        $methodName = ($methodCall->name instanceof Identifier) ? $methodCall->name->toString() : $methodCall->name;
+        switch (strtolower($methodName)) {
+            case 'getobject':
+                return $this->getTypeForGetObjectMethodCall($methodReflection, $methodCall, $scope);
+            case 'getconstructarguments':
+                return $this->getTypeForGetConstructArgumentsMethodCall($methodReflection, $methodCall, $scope);
+            case 'getcollectionmock':
+                return $this->getTypeForGetCollectionMockMethodCall($methodReflection, $methodCall, $scope);
+            default:
+                throw new ShouldNotHappenException();
+        }
+    }
+
+    /**
+     * @param MethodReflection $methodReflection
+     * @param MethodCall $methodCall
+     * @param Scope $scope
+     * @return Type
+     */
+    private function getTypeForGetObjectMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        $mixedType = new MixedType();
+        if (count($methodCall->args) === 0) {
+            return $mixedType;
+        }
+        $argType = $scope->getType($methodCall->args[0]->value);
+        if (!$argType instanceof ConstantStringType) {
+            return $mixedType;
+        }
+        return TypeCombinator::addNull(new ObjectType($argType->getValue()));
+    }
+
+    /**
+     * @param MethodReflection $methodReflection
+     * @param MethodCall $methodCall
+     * @param Scope $scope
+     * @return Type
+     */
+    private function getTypeForGetConstructArgumentsMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        return TypeCombinator::addNull(new ArrayType(new StringType(), new MixedType()));
+    }
+
+    /**
+     * @param MethodReflection $methodReflection
+     * @param MethodCall $methodCall
+     * @param Scope $scope
+     * @return Type
+     */
+    private function getTypeForGetCollectionMockMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        $className = $scope->getType($methodCall->args[0]->value)->getValue();
+        if (!is_subclass_of($className, \Magento\Framework\Data\Collection::class)) {
+            new \PHPStan\Type\MixedType();
+        }
+
+        $type = TypeCombinator::intersect(
+            new ObjectType($className),
+            new ObjectType("PHPUnit\Framework\MockObject\MockObject"),
+            new \PHPStan\Type\NullType()
+        );
+
+        return $type;
+    }
+}
