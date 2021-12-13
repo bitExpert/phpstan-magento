5c19d330384770c3c193beec0904179004b415d1
Matthew Wells <mwells@lacrossefootwear.com>
2021-12-13 12:25:26 -0500

    Generic-ize autoloader configuration
    - define autoloaders as an "extend-able" configuration
    - ensure autoloaders needing custom cache storage have it
    - warn when a container or autoloader service isn't found

--- autoload.php
+++ autoload.php
@@ -9,44 +9,25 @@
  * file that was distributed with this source code.
  */
 
-use bitExpert\PHPStan\Magento\Autoload\FactoryAutoloader;
-use bitExpert\PHPStan\Magento\Autoload\MockAutoloader;
-use bitExpert\PHPStan\Magento\Autoload\ProxyAutoloader;
-use bitExpert\PHPStan\Magento\Autoload\TestFrameworkAutoloader;
-use PHPStan\Cache\Cache;
+// This autoloader implementation supersedes the former \bitExpert\PHPStan\Magento\Autoload\Autoload implementation
+
 use PHPStan\DependencyInjection\Container;
 
 if (!isset($container) || !$container instanceof Container) {
+    echo 'No container found, or container not of expected type' . PHP_EOL;
     return;
 }
 
-// This autoloader implementation supersedes the former \bitExpert\PHPStan\Magento\Autoload\Autoload implementation
-(function (Container $container) {
+foreach ($container->getParameter('magento')['autoloaders'] as $autoloaderConfig) {
+    // see structure for magento.autoloaders in extension.neon
+    ['serviceName' => $serviceName, 'method' => $method, 'throw' => $throw, 'prepend' => $prepend] = $autoloaderConfig;
 
-    // Get the cache from PHPStan's container
-    // see https://github.com/bitExpert/phpstan-magento/pull/163#issuecomment-990926534
-    foreach ($container->findServiceNamesByType(Cache::class) as $cacheServiceName) {
-        if (!$container->hasService($cacheServiceName)) {
-            continue;
-        }
-
-        /** @var Cache|null $cache */
-        $cache = $container->getService($cacheServiceName);
-        break;
+    if (!$container->hasService($serviceName)) {
+        // warn about this
+        echo "cannot find autoloader {$serviceName}, please ensure it's configured as a service" . PHP_EOL;
+        continue;
     }
 
-    if (!isset($cache)) {
-        // TODO: throw, create our own cache?
-        return;
-    }
-
-    $mockAutoloader = new MockAutoloader();
-    $testFrameworkAutoloader = new TestFrameworkAutoloader();
-    $factoryAutoloader = new FactoryAutoloader($cache);
-    $proxyAutoloader = new ProxyAutoloader($cache);
-
-    \spl_autoload_register([$mockAutoloader, 'autoload'], true, true);
-    \spl_autoload_register([$testFrameworkAutoloader, 'autoload'], true, false);
-    \spl_autoload_register([$factoryAutoloader, 'autoload'], true, false);
-    \spl_autoload_register([$proxyAutoloader, 'autoload'], true, false);
-})($container);
+    $autoloader = $container->getService($serviceName);
+    \spl_autoload_register([$autoloader, $method], $throw, $prepend);
+}
