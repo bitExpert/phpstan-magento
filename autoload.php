cbbe26cc3a1279f702d3a8eba317475cb6e304d0
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2021-12-21 14:37:31 +0100

    Throw exception in autoloader

--- autoload.php
+++ autoload.php
@@ -15,17 +15,19 @@ use bitExpert\PHPStan\Magento\Autoload\Autoloader;
 use PHPStan\DependencyInjection\Container;
 
 if (!isset($container) || !$container instanceof Container) {
-    echo 'No container found, or container not of expected type' . PHP_EOL;
-    die(-1);
+    throw new \RuntimeException('No container found, or container not of expected type');
 }
 
 foreach ($container->getServicesByTag('phpstan.magento.autoloader') as $autoloader) {
     /** @var Autoloader|object $autoloader */
     if (!$autoloader instanceof Autoloader) {
-        echo 'Services tagged with \'phpstan.magento.autoloader\' must extend ' .
-            'bitExpert\PHPStan\Magento\Autoload\Autoloader!' . PHP_EOL .
-            get_class($autoloader) . ' does not.' . PHP_EOL;
-        die(-1);
+        throw new \RuntimeException(
+            sprintf(
+                'Service "%s" tagged with "phpstan.magento.autoloader" must implement %s!',
+                get_class($autoloader),
+                'bitExpert\PHPStan\Magento\Autoload\Autoloader'
+            )
+        );
     }
 
     $autoloader->register();
